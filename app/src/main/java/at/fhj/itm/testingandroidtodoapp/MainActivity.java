88454d0f63861d03322cd4a181e8b070a8879f04
Michael Ulm <michael.ulm@fh-joanneum.at>
2016-11-20 12:05:58 +0100

    Initial commit

---
+++ app/src/main/java/at/fhj/itm/testingandroidtodoapp/MainActivity.java
@@ -0,0 +1,78 @@
+package at.fhj.itm.testingandroidtodoapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * MainActivity of ToDo List Testing Sample
+ */
+public class MainActivity extends Activity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        // add some Items to ToDo list
+        items.add("IMS Testing Todo");
+        items.add("IMS Testing Prepare myself");
+        items.add("IMS Friday Do Awesome Lesson");
+
+        // Setup remove listener method call
+        setupListViewListener();
+
+    }
+
+    /**
+     * adds new item to ToDo List and removes current text from ToDo List
+     * @param v
+     */
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+
+    /**
+     *     Attaches a long click listener to the listview
+     */
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                        // Remove the item within array at position
+                        items.remove(pos);
+                        // Refresh the adapter
+                        itemsAdapter.notifyDataSetChanged();
+                        // Return true consumes the long click event (marks it handled)
+                        return true;
+                    }
+
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
